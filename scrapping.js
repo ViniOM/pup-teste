01cae10072faffba4d1d31f1ff5e4de316951474
Vinicios Gabriel <vini.webdeveloper@gmail.com>
2024-07-05 10:41:24 -0300

    browser puppeteer

--- scrapping.js
+++ scrapping.js
@@ -1,10 +1,21 @@
 const puppeteer = require("puppeteer");
-
+require("dotenv").config();
 const scrape = (res) => {
   try {
     (async () => {
       // Launch the browser and open a new blank page
-      const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({
+        args: [
+          "--disable-setuid-sandbox",
+          "--no-sandbox",
+          "--single-process",
+          "--no-zygote",
+        ],
+        executablePath:
+          process.env.NODE_ENV === "production"
+            ? process.env.PUPPETEER_EXECUTABLE_PATH
+            : puppeteer.executablePath(),
+      });
       const page = await browser.newPage();
 
       // Navigate the page to a URL
